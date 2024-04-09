d2df02cbdbc1cf9624eab885569d3b1bddb795cd
Rajagopal37 <trajagopal37@gmail.com>
2024-04-09 13:59:35 +0530

    2nd done

--- circle.js
+++ circle.js
@@ -1,75 +1,35 @@
+
 class Circle {
-    constructor (radius,color){
+    constructor(radius=1.0,color="red"){
         this.radius = radius;
-        this.color = color;
+        this.color = color; 
+    }
+    getRadius(){
+        return this.radius;
+    }
+    setRadius(radius){
+        this.radius=radius;
+    }
+    getColor(){
+        return this.color;
+    }
+    setColor(color){
+        this.color=this.color;
+    }
+    toString(){
+        return `Circle[radius=${this.radius},color=${this.color}]`
+    }
+    getArea(){
+        return ( Math.PI * this.radius * this.radius  );
+    }
+    getCircum(){
+        return ( 2 * Math.PI * this.radius )
     }
 }
 
-let circle = new Circle (1,"red")
-console.log(Object.entries(circle))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class details {
-//     constructor(name,age){       //constructor assign value
-//         this.peyar = name;
-//         this.vayasu = age;
-//     }            
-// }
-
-
-// let std1 = new details("Rajagopal",29);
-// let std2 = new details("Jeyakumar",29);
-// let std3 = new details("Jeyalakshmi",33);
-// let std4 = new details("Anandakumar",38);
-// let std5 = new details("Pooranamani",54);
-// let std6 = new details("Thangamariappan",59);
-
-
-// class Mobiles {
-//     constructor(a1,a2){
-//         this.name = a1;
-//         this.ram = a2;
-//     }
-//     getName (){
-//         return `My Mobile Brand is ${a1} and its a ${a2} Memory`
-//     }
-
-//     set changeName(updateRam){
-//         this.ram = updateRam
-//     }
-// }
-
-// let mob1 = new Mobiles("Samsung","8GB")
-// mob1.changeName = "16GB"
-// let mob2 = new Mobiles("BB","32GB");
-// let mob3 = new Mobiles("Nokia","8GB")
-// console.log(mob1.ram);
-
-
-
-
- 
+let circle = new Circle (5, "yellow");
+console.log(circle)
+console.log(circle.getColor())
+console.log(circle.getRadius())
+console.log(circle.getArea());
+console.log(circle.getCircum());
