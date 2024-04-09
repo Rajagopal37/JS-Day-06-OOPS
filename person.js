e292998bd25f521ae8ae97245a76f5c4ab109a45
Rajagopal37 <trajagopal37@gmail.com>
2024-04-09 14:09:19 +0530

    3 rd done

---
+++ person.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(name,mail,batch){
+        this.name = name;
+        this.mail = mail;
+        this.batch = batch;
+    }
+    toString () {
+        return `Name: ${this.name}, 
+Mail : ${this.mail},
+Batch : ${this.batch}`
+    }
+}
+
+let per01 = new Person ("ABC","abc@gmail.com","FSD-01");
+
+console.log(per01.toString())
